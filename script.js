2aaea2339823598162cb0892d055675e3471c651
Luyanda Lukhele <luylukhe021@student.wethinkcode.co.za>
2022-10-08 15:22:45 +0200

    Added tv section

--- script.js
+++ script.js
@@ -1,63 +1,125 @@
+
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+const SERIESAPI = "https://api.themoviedb.org/3/tv/popular?api_key=04c35731a5ee918f014970082a0088b1&page=1"
 
 
-getMovies(APIURL);
+window.addEventListener('load', () => {
+    const app = $('#main');
+  
+    const defaultTemplate = Handlebars.compile($('#default-template').html());
+    const moviesTemplate = Handlebars.compile($('#movies-template').html());
+    const tvTemplate = Handlebars.compile($('#tv-template').html());
+
+
+    const router = new Router({
+      mode:'hash',
+      root:'index.html',
+      page404: (path) => {
+        const html = defaultTemplate();
+        app.html(html);
+      }
+    });
+  
+    router.add('/Movies', async () => {
+        html = moviesTemplate();
+        app.html(html);
+        getMovies(APIURL);
+    });
+
+    router.add('/TV', async () => {
+        html = tvTemplate();
+        app.html(html);
+        getTV(SERIESAPI);
+      });
+  
+    router.addUriListener();
+  
+    $('a').on('click', (event) => {
+      event.preventDefault();
+      const target = $(event.target);
+      const href = target.attr('href');
+      const path = href.substring(href.lastIndexOf('/'));
+      router.navigateTo(path);
+    });
+  
+    router.navigateTo('/');
+  });
+  
 
 async function getMovies(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
 
     showMovies(respData.results);
-}
+};
 
-    function showMovies(movies) {
-        main.innerHTML = "";
 
-        movies.forEach((movie) => {
-            const {poster_path, title, vote_average, overview} = movie;
-    
-            const movieEl = document.createElement("div");
-            movieEl.classList.add("movie");
-    
-            movieEl.innerHTML = `
-                <img src="${IMGPATH + poster_path}" alt="${title}"/>
-                <div class="movie-info">
-                    <h3>${title}</h3>
-                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-                </div>
-                <div class="overview">
-                <h3>Overview: </h3>
-                    ${overview}
-                </div>
-                `;
-    
-            main.appendChild(movieEl);
-        });
+async function getTV(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    showTV(respData.results);
+};
+
+
+function showTV(tvShows) {
+    main.innerHTML = "";
+
+    tvShows.forEach((movie) => {
+        const {poster_path, name, vote_average, overview} = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${name}"/>
+            <div class="movie-info">
+                <h3>${name}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+            <h3>${name}: </h3>
+                ${overview}
+            </div>
+            `;
+
+        main.appendChild(movieEl);
+    });
+};
+
+  
+function showMovies(movies) {
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const {poster_path, title, vote_average, overview} = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${title}"/>
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+            <h3>${title}: </h3>
+                ${overview}
+            </div>
+            `;
+
+        main.appendChild(movieEl);
+    });
+};
+
+function getClassByRate(vote) {
+    if (vote >= 7.5) {
+        return "green";
+    }else if (vote >=5) {
+        return "orange";
+    }else {
+        return "red";
     }
-
-    function getClassByRate(vote) {
-        if (vote >= 8) {
-            return "green";
-        }else if (vote >=5) {
-            return "orange";
-        }else {
-            return "red";
-        }
-    }
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const searchTerms = search.value;
-
-    if(searchTerms) {
-        getMovies(SEARCHAPI + searchTerms);
-
-        search.value = "";
-    }
-});
+};
